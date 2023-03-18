f3cbe5e394fa1ff81a101ebefdf1bab509da647d
Mahbub-ur <mahbuburrahman5658@gmail.com>
2023-03-18 22:17:46 +0600

    countdown done but need some extra feture on js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,53 @@
-var countDownDate = new Date("22-Mar-23 00:00:00").getTime();
+var countDownDate = new Date("19-Mar-23 00:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
-
+    
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+    
+    let d = days < 10 ? "0" + days : days;
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    document.getElementById("days").innerHTML = d;
+    document.getElementById("hours").innerHTML = h;
+    document.getElementById("minutes").innerHTML = m;
+    document.getElementById("seconds").innerHTML = s;
 },1000);
+
+let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+function stopwatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    displayTime.innerHTML = h +":"+ m +":"+ s;
+}
+function watchStart(){
+    if(timer!== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+
+}
+function watchStop(){
+    clearInterval(timer);
+}
+function watchReset(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+}
